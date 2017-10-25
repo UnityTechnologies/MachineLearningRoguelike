57bf9f9eaf59f1c0327d17797bad05bc073386eb
Ciro Continisio <ciro@unity3d.com>
2017-10-25 19:57:11 +0100

    Loads of colliders, initial test with layout Character controller test

---
+++ Assets/Scripts/SimpleCharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		float hor = Input.GetAxis("Horizontal");
+		float ver = Input.GetAxis("Vertical");
+
+		rb.AddForce(new Vector2(hor, ver) * speed);
+	}
+}
